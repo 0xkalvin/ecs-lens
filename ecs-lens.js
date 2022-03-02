33265f09883ae05c3b0e65537d8b071c1e66c07f
Kalvin Vasconcellos <kalvin.vasconcellos@gmail.com>
2022-03-02 18:33:11 -0300

    add initial implementation

---
+++ ecs-lens.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
+const output = require('./lib/output')
+const ecs = require('./lib/ecs')
+
+function parseOptions (rawOptions) {
+  const { cluster, region } = rawOptions
+
+  return {
+    cluster,
+    region
+  }
+}
+
+async function run (options) {
+  const parsedOptions = parseOptions(options)
+
+  output.printInitialMessage(parsedOptions)
+
+  const deployments = await ecs.getDeploments(parsedOptions)
+
+  const table = output.buildResultTable(deployments)
+
+  console.log(table.toString())
+}
+
+if (require.main === module) {
+  const argv = yargs(hideBin(process.argv))
+    .usage('Usage: ecs-lens [options]')
+    .example('ecs-lens --region us-east-1 --cluster mycluster')
+    .alias('r', 'region')
+    .nargs('r', 1)
+    .describe('r', 'AWS region')
+    .demandOption(['r'])
+    .alias('c', 'cluster')
+    .nargs('c', 1)
+    .describe('c', 'ECS cluster name')
+    .demandOption(['c'])
+    .alias('v', 'version')
+    .help('h')
+    .alias('h', 'help')
+    .argv
+
+  run(argv)
+}
+
+module.exports.run = run
